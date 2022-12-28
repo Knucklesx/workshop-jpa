3623455e9c5535e8be7563a3f0592e444ce74a58
Knucklesx <gcfacchi@gmail.com>
2022-12-28 09:33:37 -0300

    FEAT: Created category entitie, service and resource

---
+++ src/main/java/com/knuckles/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.knuckles.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.knuckles.course.entities.Category;
+import com.knuckles.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+
+	}
+}
